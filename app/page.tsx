2e4d328b85e586b81b86a0f9026c4cff54a3aeb5
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-08-04 10:14:25 +0530

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import { Cards } from '@/components/persional/cards';
 import { HeroSection } from '@/components/persional/hero-section'
 import { NavbarTop } from '@/components/persional/navbar'
 import React from 'react'
 
 export default function page() {
+  const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
+`;
   return (
     <>
       <NavbarTop />
       <HeroSection />
+      <h1 className="text-4xl text-center mt-5 border-b pb-4">All Tools</h1>
+      <div className='flex flex-wrap justify-evenly gap-2.5 px-5 '>
+        {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,].map((item)=>{
+          return <Cards key={item} />
+        })}
+      </div>
     </>
   )
 }
--- components/persional/hero-section.tsx
+++ components/persional/hero-section.tsx
@@ -1,20 +1,36 @@
 "use client";
+import { motion } from "motion/react";
 import React from "react";
-import { Spotlight } from "@/components/ui/spotlight-new";
+import { ImagesSlider } from "../ui/images-slider";
 
 export function HeroSection() {
+  const images = [
+    "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Spotlight <br /> which is not overused.
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          A subtle yet effective spotlight effect, because the previous version
-          is used a bit too much these days.
-        </p>
-      </div>
-    </div>
+    <ImagesSlider className="h-[40rem]" images={images}>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative z-50 flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          Background lights are cool you know.
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          And this, is chemical burn.
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Debug now
+        </button>
+      </motion.div>
+    </ImagesSlider>
   );
 }
